291638f9cce8ba4080fed342a322598603c297cf
sushi0977 <sushi0907@comcast.net>
2017-06-28 22:39:45 -0600

    Assignment4.3

--- index.php
+++ index.php
@@ -11,24 +11,40 @@ $name = "Kilroy";
 $stmt->bindParam(":asin", $asin);
 $stmt->execute();*/
 
-fInsertToDatabase('B01MAZGLMP', 'Moana', 19.99);
-fInsertToDatabase('B01LTHOAGM', 'Fantastic Beasts and Where to Find Them', 24.51);
-fInsertToDatabase('B00N1JQ452', 'Guardians Of The Galaxy', 19.96);
+//fInsertToDatabase('B01MAZGLMP', 'Moana', 19.99);
+//fInsertToDatabase('B01LTHOAGM', 'Fantastic Beasts and Where to Find Them', 24.51);
+//fInsertToDatabase('B00N1JQ452', 'Guardians Of The Galaxy', 19.96);
 //fDeleteFromDatabase('B01MAZGLMP');
 fListFromDatabase();
 
 fInsertActorsToDatabase( 'Chris', 'Pratt');
 fInsertActorsToDatabase( 'Zoe', 'Saldana');
-fInsertActorsToDatabase( 'Joe', 'Gunn');
+fInsertActorsToDatabase( 'Alulil', 'Cravalho');
 fInsertActorsToDatabase( 'Dwayne', 'Johnson');
 fInsertActorsToDatabase('Eddie','Redmayne');
 fInsertActorsToDatabase('Katherine', 'Waterston');
 fInsertActorsToDatabase('Emma','Watson');
 fInsertActorsToDatabase('Dan', 'Stevens');
-fInsertActorsToDatabase('Russell', "Crowe");
+fInsertActorsToDatabase('Russell', 'Crowe');
 fInsertActorsToDatabase('Joaquin', 'Phoenix');
 
 fListToDatabase();
+
+
+/*fInsertCrosswalk('B00AEFY552', '29');
+fInsertCrosswalk('B00AEFY552', '30');
+fInsertCrosswalk('B00N1JQ452', '21');
+fInsertCrosswalk('B00N1JQ452', '22');
+fInsertCrosswalk('B01LTHOAGM', '25');
+fInsertCrosswalk('B01LTHOAGM', '26');
+fInsertCrosswalk('B01MAZGLMP', '24');
+fInsertCrosswalk('B01MAZGLMP', '63');
+fInsertCrosswalk('B01MZIQ2WO', '27');
+fInsertCrosswalk('B01MZIQ2WO', '28');*/
+
+
+fListDVDTitleAndActors();
+
 
 //$stmt = $myDB->prepare("SELECT * FROM test");
 /*$stmt = $myDB->prepare("SELECT * FROM dvdtitles");
